56074ac62fce44e3573c538990c3093addab36fd
Aashish7710 <choudharyaashish613@gmail.com>
2026-06-01 16:43:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>chai aur code</h1>
+      <h1>count is 5</h1>
+      <button>add</button>
+      <button>remove</button>
+    </>
+  )
+}
+
+export default App
